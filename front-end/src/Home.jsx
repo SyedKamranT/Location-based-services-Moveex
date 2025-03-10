2ecad45338ab94433ca23f33e42d4379a0fc471d
Syed Kamran <Unitygame1380@gmail.com>
2025-03-10 16:52:43 +0530

    updating

--- front-end/src/Home.jsx
+++ front-end/src/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import backgroundImg from './assets/background.png'
+import backgroundImg from './assets/backgroundImg.png'
 import worker from './assets/worker.png'
 import factoryimage from './assets/factory.png'
 import { CiSearch } from "react-icons/ci";
@@ -36,11 +36,11 @@ const Home = () => {
           </div>
           {/* right div */}
           <div className=' w-1/2 mt-[84px]'>
-            <h1 className=' marcellus  text-[96px] text-white leading-[122px] tracking-[0.25px] '>Moving Fast,<br /> Moving Smart</h1>
-            <p className=' text-white mt-5 w-[80%]'>Reliable, efficient, and hassle-free deliveries with real-time tracking. We move your packages swiftly and securely, every time.</p>
-            <div className=' flex justify-between items-center bg-white rounded-[8px] mt-11 px-5 w-[80%]  '>
+            <h1 className=' marcellus-bold  text-[96px] text-white leading-[122px] tracking-[0.25px] '>Moving Fast,<br /> Moving Smart</h1>
+            <p className=' text-white mt-5 w-[90%] poppins-light'>Reliable, efficient, and hassle-free deliveries with real-time tracking. We move your packages swiftly and securely, every time.</p>
+            <div className=' flex justify-between items-center bg-white rounded-[8px] mt-11 px-2 pl-5 w-[80%]  '>
               <input className='w-[572px] h-[70px] outline-none ' type="text" name="" id="" placeholder='Enter city or pincode' />
-              <button className='cursor-pointer bg-[#0F1521] text-white w-[40%] flex justify-center py-3.5 gap-1 rounded-[6px]  '><span className='poppins-medium text-[16px]  tracking-[1px]'>Search</span> <CiSearch className='poppins-medium text-[20px] font-extrabold' /> </button>
+              <button className='cursor-pointer bg-[#0F1521] text-white w-[40%] flex justify-center py-4.5 gap-2 rounded-[6px] hover:bg-[#1d1d1d] '><span className='poppins-medium text-[14px]  tracking-[1px]'>Search</span> <CiSearch className='poppins-medium text-[20px] font-extrabold' /> </button>
             </div>
           </div>
 
@@ -49,15 +49,15 @@ const Home = () => {
 
       </div>
       {/* white section */}
-      <div className=' w-[80%] mx-auto bg-[#133BB7]/10 shadow-2xl h-[599px]  my-[100px] rounded-[16px] flex justify-between'>
+      <div className=' w-[80%] mx-auto bg-[#133BB7]/10 shadow-xl h-[599px]  my-[100px] rounded-[16px] flex justify-between'>
         <div className='flex justify-center flex-col gap-5  mx-[60px] '>
           <div className='bg-white w-[130px] h-[40px] text-[#133BB7] text-[16px] poppins-semibold rounded-[8px] flex justify-center items-center'><li>About Us</li></div>
           <div className='poppins-semibold text-[36px] leading-[50px] tracking-[0.25px]'>The future of <br />
             fast, smart, and reliable <br />
             <span className=' text-[#133BB7]'>delivery starts here.</span> </div>
-          <div><button className=' bg-[#133BB7] text-white w-[154px] h-[60px] rounded-[8px] border-none poppins-medium text-[16px] cursor-pointer'>Learn More</button></div>
+          <div><button className=' bg-[#133BB7] text-white w-[154px] h-[60px] rounded-[8px] border-none poppins-medium text-[16px] cursor-pointer hover:bg-[#2352e0] hover:shadow-xl transition-all ease-in delay-25'>Learn More</button></div>
         </div>
-        <div className='w-[572px] h-[598px]'>
+        <div className='w-[572px] h-[598px]  '>
           <img src={factoryimage} alt="" />
         </div>
       </div>
@@ -67,18 +67,18 @@ const Home = () => {
         <div className='w-[80%] mx-auto flex flex-col gap-[60px]'>
           <h1 className=' poppins-semibold text-[48px] text-white text-center leading-[70px]'>We deliver unmatched speed and <br /> reliability for every shipment.</h1>
           <div className=' flex justify-between '>
-            <div className=' flex justify-center items-center flex-col '>
-              <div><img className=' w-[335px] h-[381px] ' src={twowheel} alt="" /></div>
+            <div className=' flex justify-center items-center flex-col hover:translate-y-2 ease-in delay-25 transition-all '>
+              <div><img className=' w-[335px] h-[381px] hover:shadow-xl transition-all ease-in delay-25  ' src={twowheel} alt="" /></div>
               <div className='relative bottom-10 flex justify-center items-center bg-white rounded-full w-20 h-20'><img className='w-10 h-10' src={bikeicon} alt="" /></div>
 
             </div >
-            <div className=' flex justify-center items-center flex-col'>
-              <div><img className=' w-[335px] h-[381px]' src={fourwheel} alt="" /></div>
+            <div className=' flex justify-center items-center flex-col hover:translate-y-2 ease-in delay-25 transition-all '>
+              <div><img className=' w-[335px] h-[381px] hover:shadow-xl transition-all ease-in delay-25' src={fourwheel} alt="" /></div>
               <div className='relative bottom-10 flex justify-center items-center bg-white rounded-full w-20 h-20' ><img className='w-10 h-10' src={caricon} alt="" /></div>
 
             </div>
-            <div className=' flex justify-center items-center flex-col'>
-              <div><img className=' w-[335px] h-[381px]' src={twelvewheel} alt="" /></div>
+            <div className=' flex justify-center items-center flex-col hover:translate-y-2 ease-in delay-25 transition-all'>
+              <div><img className=' w-[335px] h-[381px] hover:shadow-xl transition-all ease-in delay-25' src={twelvewheel} alt="" /></div>
               <div className='relative bottom-10 flex justify-center items-center bg-white rounded-full w-20 h-20'><img className='w-10 h-10' src={truckicon} alt="" /></div>
 
             </div>
@@ -89,7 +89,7 @@ const Home = () => {
 
       </div>
       {/* white section */}
-      <div className=' w-[80%] mx-auto bg-[#133BB7]/10 shadow-2xl h-[599px]  my-[100px] rounded-[16px] flex justify-start gap-[90px] '>
+      <div className=' w-[80%] mx-auto bg-[#133BB7]/10 shadow-xl h-[599px]  my-[100px] rounded-[16px] flex justify-start gap-[90px] '>
 
         <div className='w-[572px] h-[598px]'>
           <img src={manytrucks} alt="" />
@@ -99,7 +99,7 @@ const Home = () => {
           <div className='poppins-semibold text-[36px] leading-[50px] tracking-[0.25px]'>Simple procedures for receiving <br />
 
             <span className=' text-[#133BB7]'>your shipment.</span> </div>
-          <div><button className=' bg-[#133BB7] text-white w-[154px] h-[60px] rounded-[8px] border-none poppins-medium text-[16px] cursor-pointer'>Learn More</button></div>
+          <div><button className=' bg-[#133BB7] text-white w-[154px] h-[60px] rounded-[8px] border-none poppins-medium text-[16px] cursor-pointer hover:bg-[#2352e0] hover:shadow-xl transition-all ease-in delay-25'>Learn More</button></div>
         </div>
       </div>
 
@@ -112,7 +112,7 @@ const Home = () => {
             <p className=' poppins-regular text-[18px]'>Get in touch,</p>
             <div className='flex justify-between bg-transparent border-[2px] border-white w-[488px] py-[10px] pl-10 pr-[12px] rounded-[8px]   '>
               <input className='outline-none poppins-regular text-[18px]' type="text" name="" id="" placeholder='Enter your email address' />
-              <button className='bg-white w-[171px] h-[54px] text-[#133BB7] rounded-[4px] poppins-medium text-[18px] cursor-pointer '>Join Now </button>
+              <button className='bg-white w-[171px] h-[54px] text-[#133BB7] rounded-[4px] poppins-medium text-[18px] cursor-pointer hover:bg-[#2352e0] hover:text-white hover: transition-all ease-in delay-25 '>Join Now </button>
             </div>
           </div>
           <div>
@@ -147,12 +147,12 @@ const Home = () => {
       </div>
       <div className=' w-[80%] h-[45px] text-[#133BB7] mx-auto flex justify-between items-center '>
         <p className='poppins-regular text-[14px]'>© 2011 All Rights Reserved Moveex Pvt Ltd</p>
-      
+
         <div className=' flex items-center flex-row gap-2 text-[#133BB7]'>
           <NavLink className=" poppins-regular text-[14px] tracking-[0.5px]" to="/">Home page </NavLink> |
           <NavLink className="poppins-regular text-[14px] tracking-[0.5px]" to="/contact">Contact</NavLink> |
-          <NavLink className=" poppins-regular text-[14px] tracking-[0.5px]" to="/">Privacy policy </NavLink> 
-         </div>
+          <NavLink className=" poppins-regular text-[14px] tracking-[0.5px]" to="/">Privacy policy </NavLink>
+        </div>
       </div>
 
     </div>
--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -7,19 +7,19 @@ const Navbar = () => {
   return (
     <div className=' text-white bg-black/40 backdrop-blur-md mx-5 rounded-[9px] '>
       <div className=' flex justify-between items-center p-5'>
-        <div><img  className=' w-[181px] h-[30px] cursor-pointer' src={logo} alt="" /></div>
+        <div><img  className=' w-[181px]  cursor-pointer' src={logo} alt="" /></div>
         <div>
           <nav>
          <ul className=' flex gap-10'>
-          <li><NavLink className=" outline-none border-none hover:text-[#133BB7]" to="/aboutus">ABOUT US</NavLink></li>
-          <li><NavLink className=" outline-none border-none hover:text-[#133BB7]" to="/contact">CONTACT</NavLink></li>
-          <li><NavLink className=" outline-none border-none hover:text-[#133BB7]" to="/courierer">BECOME A COURIERER</NavLink></li>
+          <li><NavLink className=" outline-none text-sm border-none   poppins-medium tracking-[3px] hover:underline underline-offset-5  " to="/aboutus">ABOUT US</NavLink></li>
+          <li><NavLink className=" outline-none text-sm border-none  poppins-medium tracking-[3px] hover:underline underline-offset-5" to="/contact">CONTACT</NavLink></li>
+          <li><NavLink className=" outline-none text-sm border-none  poppins-medium tracking-[3px] hover:underline underline-offset-5" to="/courierer">BECOME A COURIER</NavLink></li>
          </ul>
           </nav>
         </div>
         <div className=' flex gap-5'>
-          <button className='w-[119px] h-[50px] border-[2px] border-[#133BB7]  text-white rounded-[9px] cursor-pointer '>LOGIN</button>
-          <button className='w-[119px] h-[50px] bg-[#133BB7] text-white rounded-[9px] cursor-pointer '>SIGNUP</button>
+          <button className='w-[119px] text-sm h-[50px] border-[2px] border-white text-white rounded-[9px] cursor-pointer poppins-medium tracking-[2px] hover:bg-[#133BB7] hover:border-none hover:shadow-xl transition-all ease-in delay-25 '>LOGIN</button>
+          <button className='w-[119px] text-sm h-[50px] bg-[#133BB7] text-white rounded-[9px] cursor-pointer poppins-medium tracking-[2px] hover:bg-white hover:text-[#133BB7] hover:shadow-xl transition-all ease-in delay-25'>SIGNUP</button>
         </div>
       </div>
     </div>
